bcd5c27c8bddc0081410a99ff4258b1fcebc8917
Marcel Breuer <breuer.marcel@outlook.com>
2025-04-18 17:30:37 +0200

    Update siteUrl in next-sitemap.config.js to reflect the correct domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://cv.marcel-breuer.net',
+    siteUrl: 'https://marcel-breuer.net',
     generateRobotsTxt: true,
     robotsTxtOptions: {
         policies: [
