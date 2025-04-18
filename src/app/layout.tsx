57c48b9684c1b9573667abaaf84c6ce1ddd19986
Marcel Breuer <breuer.marcel@outlook.com>
2025-04-18 16:27:36 +0200

    Refactor layout and globals styles for improved readability and consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
+import { Metadata } from 'next'
 import './globals.css'
+import { Sen } from 'next/font/google'
+
 import { ReactNode } from 'react'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Marcel Breuer | Curriculum Vitae',
   description: 'IT-Manager, Business Intelligence Expert, Data Scientist',
 }
+
+const font = Sen({ subsets: ['latin'] })
 
 interface RootLayoutProps {
   children: ReactNode
@@ -13,7 +18,9 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="de">
-      <body className="bg-white text-gray-900 antialiased dark:bg-gray-900 dark:text-gray-100">
+      <body
+        className={`${font.className} bg-white text-gray-900 antialiased dark:bg-gray-900 dark:text-gray-100 `}
+      >
         {children}
       </body>
     </html>
